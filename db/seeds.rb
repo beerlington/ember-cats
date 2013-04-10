caac791d72e631ce5e042b7b16e4e58d1b10ab35
beerlington <pete@lette.us>
2013-04-09 23:04:52 -0400

    Cleaning up seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,28 +1,18 @@
 Cat.destroy_all
 
-
 cats = [
-  'Main Coon',
-  'Persian',
-  'Siamese',
-  'Bengal',
-  'Savannah',
-  'Ragdoll'
+  'Sofia',
+  'Portia',
+  'Smiles',
+  'Baby',
+  'Abbott',
+  'Lobster',
+  'Ajax'
 ]
 
 cats.each do |cat|
   Cat.create!(
     breed: cat,
-    image: "#{cat.underscore}.png"
+    image: "#{cat.underscore}.jpg"
   )
 end
-
-Dog.create!(
-  age: 4,
-  breed: 'Golden Retriever'
-)
-
-Dog.create!(
-  age: 3,
-  breed: 'Husky'
-)
